31f8c6dd1706ecf470ec776222bdedb720552dc8
singhzrepo <contactwithsid@gmail.com>
2022-11-19 07:43:10 +0530

    Added crud API

--- index.js
+++ index.js
@@ -3,16 +3,60 @@ const PORT = 8000;
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.end("This is served by express")
+app.use(express.json())
+app.use(express.urlencoded())
+
+// CRUD ... 
+
+const students = [ 
+    {
+        name: 'abcd',
+        roll: 12,
+    },
+    {
+        name: 'abcde',
+        roll: 13,
+    },
+    {
+        name: 'abcdef',
+        roll: 14,
+    }
+ ]
+
+app.get('/getStudents', (req, res) => {
+
+    return res.status(200).json({
+        data: students
+    })
+
 })
 
-app.get('/home', (req, res) => {
-    return res.sendFile(__dirname + '/index.html')
+app.post('/addStudent', (req, res) => {
+
+    console.log(req.body)
+    students.push(req.body)
+    return res.status(200).json({
+        data: students
+    })
+
 })
 
-app.get('/about', (req, res) => {
-    return res.end("This is served by express, about!")
+app.put('/updateStudent/:roll', (req, res) => {
+    console.log(req.params.roll)
+    const roll = parseInt(req.params.roll)
+
+    // find the student roll, 
+
+    const index =  students.findIndex((student) => student.roll === roll)
+    if(index !== -1){
+        students.splice(index, 1, req.body)
+    }else {
+        students.push(req.body)
+    }
+
+    return res.status(200).json({
+        data: students
+    })
 })
 
 
